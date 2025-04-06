bc15bd1dcd35f6e9f5f735a57cc2f602ba7059b0
MKaleeswari <kaleeswari2220@gmail.com>
2025-04-06 16:08:58 +0530

    changes

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-
 import avatar1 from "../assets/images/avatar1.png";
 import avatar2 from "../assets/images/avatar1.png";
 import avatar3 from "../assets/images/avatar1.png";
