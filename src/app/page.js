5ac28e09ee558255bd48e84fdb2b579d3bd13156
MKaleeswari <kaleeswari2220@gmail.com>
2025-04-06 15:42:45 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Hero from "@/components/Hero";
-import Navbar from "@/Components/Navbar";
-import About from "@/components/About";
-import Portfolio from "@/components/Portfolio";
-import Testimonials from "@/components/Testimonials";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+import Hero from "../Components/Hero";
+import Navbar from "../Components/Navbar";
+import About from "../components/About";
+import Portfolio from "../components/Portfolio";
+import Testimonials from "../components/Testimonials";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
